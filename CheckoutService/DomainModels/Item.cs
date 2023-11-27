e0c40907fc7e9052aa439593893116e6fac6ea3f
Sathish <sathish.nagula@outlook.com>
2023-11-27 13:40:31 +0000

    Added domain models

---
+++ CheckoutService/DomainModels/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutService.DomainModels
+{
+    public class Item
+    {
+        public string SKU { get; set; }
+        public decimal Price { get; set; } 
+    }
+}
